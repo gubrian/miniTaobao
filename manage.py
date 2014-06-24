4915d4b84c0d74a8e27c079b5e3e7189d78ea445
gubrian <gubrian@gubrian.local>
2014-06-23 23:57:29 -0700

    test

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-
+#it will be nice if you could do more
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miniTaobao.settings")
 
